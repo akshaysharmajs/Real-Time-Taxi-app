1a85cef3c53fc8b520f31c5dffde21db5beb283b
Akshay Sharma <42249933+akshaysharmajs@users.noreply.github.com>
2023-04-09 15:44:06 -0400

    Part 3 - Testing Loading Trips

--- taxi-react-app/client/cypress/e2e/driver.cy.js
+++ taxi-react-app/client/cypress/e2e/driver.cy.js
@@ -9,11 +9,102 @@ const riderEmail = faker.internet.email();
 const riderFirstName = faker.name.firstName();
 const riderLastName = faker.name.lastName();
 
+const tripResponse = [
+  {
+    id: '94fc5eba-de7a-44b2-8000-856ec824609d',
+    created: '2020-08-18T21:41:08.112946Z',
+    updated: '2020-08-18T21:41:08.112986Z',
+    pick_up_address: 'A',
+    drop_off_address: 'B',
+    status: 'STARTED',
+    driver: {
+      id: 113,
+      first_name: driverFirstName,
+      last_name: driverLastName,
+      photo: 'http://localhost:8003/media/photos/photo_QI0TTYh.jpg',
+    },
+    rider: {
+      id: 112,
+      first_name: riderFirstName,
+      last_name: riderLastName,
+      photo: 'http://localhost:8003/media/photos/photo_r3XrvgH.jpg',
+    }
+  },
+  {
+    id: 'bb3042cd-88dd-472c-890f-c5f59481de01',
+    created: '2020-08-18T21:41:08.112946Z',
+    updated: '2020-08-18T21:41:08.112986Z',
+    pick_up_address: 'A',
+    drop_off_address: 'B',
+    status: 'REQUESTED',
+    driver: {
+      id: 113,
+      first_name: driverFirstName,
+      last_name: driverLastName,
+      photo: 'http://localhost:8003/media/photos/photo_QI0TTYh.jpg',
+    },
+    rider: {
+      id: 112,
+      first_name: riderFirstName,
+      last_name: riderLastName,
+      photo: 'http://localhost:8003/media/photos/photo_r3XrvgH.jpg',
+    }
+  },
+  {
+    id: '50e0034f-0696-4b26-9068-8a7d064db922',
+    created: '2020-08-18T21:41:08.112946Z',
+    updated: '2020-08-18T21:41:08.112986Z',
+    pick_up_address: 'A',
+    drop_off_address: 'B',
+    status: 'COMPLETED',
+    driver: {
+      id: 113,
+      first_name: driverFirstName,
+      last_name: driverLastName,
+      photo: 'http://localhost:8003/media/photos/photo_QI0TTYh.jpg',
+    },
+    rider: {
+      id: 112,
+      first_name: riderFirstName,
+      last_name: riderLastName,
+      photo: 'http://localhost:8003/media/photos/photo_r3XrvgH.jpg',
+    }
+  }
+];
+
+
 describe('The driver dashboard', function () {
   // new
   before(function () {
     cy.addUser(riderEmail, riderFirstName, riderLastName, 'rider');
     cy.addUser(driverEmail, driverFirstName, driverLastName, 'driver');
+  });
+
+  it('Displays current, requested, and completed trips', function () {
+    cy.intercept('trip', {
+      statusCode: 200,
+      body: tripResponse
+    }).as('getTrips');
+  
+    cy.logIn(driverEmail);
+  
+    cy.visit('/#/driver');
+    cy.wait('@getTrips');
+  
+    // Current trips.
+    cy.get('[data-cy=trip-card]')
+      .eq(0)
+      .contains('STARTED');
+  
+    // Requested trips.
+    cy.get('[data-cy=trip-card]')
+      .eq(1)
+      .contains('REQUESTED');
+  
+    // Completed trips.
+    cy.get('[data-cy=trip-card]')
+      .eq(2)
+      .contains('COMPLETED');
   });
 
   it('Cannot be visited if the user is not a driver', function () {
@@ -33,4 +124,28 @@ describe('The driver dashboard', function () {
     cy.visit('/#/driver');
     cy.hash().should('eq', '#/driver');
   });
+
+
+it('Displays messages for no trips', function () {
+  cy.intercept('trip', {
+    statusCode: 200,
+    body: []
+  }).as('getTrips');
+
+  cy.logIn(riderEmail);
+
+  cy.visit('/#/rider');
+  cy.wait('@getTrips');
+
+  // Current trips.
+  cy.get('[data-cy=trip-card]')
+    .eq(0)
+    .contains('No trips.');
+
+  // Completed trips.
+  cy.get('[data-cy=trip-card]')
+    .eq(1)
+    .contains('No trips.');
+  });
+
 });
--- taxi-react-app/client/cypress/e2e/rider.cy.js
+++ taxi-react-app/client/cypress/e2e/rider.cy.js
@@ -8,12 +8,76 @@ const driverLastName = faker.name.lastName();
 const riderEmail = faker.internet.email();
 const riderFirstName = faker.name.firstName();
 const riderLastName = faker.name.lastName();
+const tripResponse = [
+  {
+    id: '23033964-f2ca-4150-bb8c-4a1182c3737d',
+    created: '2020-08-18T21:41:08.112946Z',
+    updated: '2020-08-18T21:41:08.112986Z',
+    pick_up_address: 'A',
+    drop_off_address: 'B',
+    status: 'STARTED',
+    driver: {
+      id: 113,
+      first_name: driverFirstName,
+      last_name: driverLastName,
+      photo: 'http://localhost:8003/media/photos/photo_QI0TTYh.jpg',
+    },
+    rider: {
+      id: 112,
+      first_name: riderFirstName,
+      last_name: riderLastName,
+      photo: 'http://localhost:8003/media/photos/photo_r3XrvgH.jpg',
+    }
+  },
+  {
+    id: '2ee84fb5-f3c4-4aff-9677-b6476156d3bf',
+    created: '2020-08-18T21:41:08.112946Z',
+    updated: '2020-08-18T21:41:08.112986Z',
+    pick_up_address: 'A',
+    drop_off_address: 'B',
+    status: 'COMPLETED',
+    driver: {
+      id: 113,
+      first_name: driverFirstName,
+      last_name: driverLastName,
+      photo: 'http://localhost:8003/media/photos/photo_QI0TTYh.jpg',
+    },
+    rider: {
+      id: 112,
+      first_name: riderFirstName,
+      last_name: riderLastName,
+      photo: 'http://localhost:8003/media/photos/photo_r3XrvgH.jpg',
+    }
+  }
+];
 
 describe('The rider dashboard', function () {
   // new
   before(function () {
     cy.addUser(riderEmail, riderFirstName, riderLastName, 'rider');
     cy.addUser(driverEmail, driverFirstName, driverLastName, 'driver');
+  });
+
+  it('Displays current and completed trips', function () {
+    cy.intercept('trip', {
+      statusCode: 200,
+      body: tripResponse
+    }).as('getTrips');
+  
+    cy.logIn(riderEmail);
+  
+    cy.visit('/#/rider');
+    cy.wait('@getTrips');
+  
+    // Current trips.
+    cy.get('[data-cy=trip-card]')
+      .eq(0)
+      .contains('STARTED');
+  
+    // Completed trips.
+    cy.get('[data-cy=trip-card]')
+      .eq(1)
+      .contains('COMPLETED');
   });
 
   it('Cannot be visited if the user is not a rider', function () {
@@ -33,4 +97,27 @@ describe('The rider dashboard', function () {
     cy.visit('/#/rider');
     cy.hash().should('eq', '#/rider');
   });
+
+  it('Displays messages for no trips', function () {
+    cy.intercept('trip', {
+      statusCode: 200,
+      body: []
+    }).as('getTrips');
+  
+    cy.logIn(riderEmail);
+  
+    cy.visit('/#/rider');
+    cy.wait('@getTrips');
+  
+    // Current trips.
+    cy.get('[data-cy=trip-card]')
+      .eq(0)
+      .contains('No trips.');
+  
+    // Completed trips.
+    cy.get('[data-cy=trip-card]')
+      .eq(1)
+      .contains('No trips.');
+  });
+
 });
